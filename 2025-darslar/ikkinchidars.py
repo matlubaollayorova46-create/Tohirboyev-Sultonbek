14e5827a808dc81179bac0adf84ac184dd2b129c
Humoyun009 <otaxonovmadad25@gmail.com>
2025-10-27 16:11:47 +0500

    Ro'yhatlar bilan ishlsh

---
+++ 2025-darslar/ikkinchidars.py
@@ -0,0 +1,48 @@
+# Print bilan ishlash
+#print("salom barchaga aziz o'quvchilar")
+#print('Hayir o\'quvchilar')
+#print("Hayir \n o'quv \n chilar")
+#print("Barchangiz darsga tayyorgarlik \n ko'rib kelinglar")
+#print("""salomhammaga darslar zo'r""")
+
+#Qo'shish
+#a = 18
+#b = 12
+#c = a + b
+#print('c =', c)
+
+#Ayirish
+#z = 120 + 2
+#s = 23
+#x = z - s
+#print('x =', x)
+
+#Qo'shish
+#b = 100
+#f = 200
+#q = b + f
+#print('q =', q)
+
+#Ko'paytirish
+#b = 2
+#n = 3
+#z = b * n
+#print('z =', z)
+
+#Bo'lish
+#c = 4
+#e = 2
+#n = c / e
+#print('n =', n)
+
+#Bo'lish
+c = 5
+e = 2
+n = c // e
+print('n =', n)
+
+#Ko'paytirish
+c = 4
+e = 2
+n = c ** e
+print('n =', n)
